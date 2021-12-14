c13243d21a5792dde57b6b5a6f887498ba7eef62
bxxmi <bmi6638@gmail.com>
2021-12-14 21:57:00 +0900

    feat: add react-error-boundary

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,24 @@ import SignIn from "./components/SignIn";
 import DetailBook from "./components/DetailBook";
 import EditBook from "./components/EditBook";
 import NotFound from "./components/NotFound";
+import Error from "./components/Error";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/add" element={<AddBooks />} />
-        <Route path="/book/:id" element={<DetailBook />} />
-        <Route path="/edit/:id" element={<EditBook />} />
-        {/* 404 page */}
-        <Route element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/add" element={<AddBooks />} />
+          <Route path="/book/:id" element={<DetailBook />} />
+          <Route path="/edit/:id" element={<EditBook />} />
+          {/* 404 page */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
