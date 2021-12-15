727b5412d8165993bb30f5261fba9336105c8084
bxxmi <bmi6638@gmail.com>
2021-12-15 14:26:04 +0900

    design: sigin page design

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { Col, Input, Row, Button } from "antd";
+import styles from "./Signin.module.css";
 
 const SignIn = () => {
-  return <div>SIGNIN</div>;
+  return (
+    <Row align="middle" className={styles.signin_row}>
+      <Col span={24}>
+        <Row className={styles.signin_contents}>
+          <Col span={12}>
+            <img
+              src="/bg_signin.png"
+              alt="이미지"
+              className={styles.signin_bg}
+            />
+          </Col>
+          <Col span={12}>
+            <div className={styles.signin_title}>My Books</div>
+            <div className={styles.signin_subtitle}>
+              Please Note Your Opinion
+            </div>
+            <div className={styles.signin_underline} />
+            <div className={styles.email_title}>
+              Email
+              <span className={styles.required}>*</span>
+            </div>
+            <div className={styles.input_area}>
+              <Input
+                placeholder="Email"
+                autoComplete="email"
+                name="email"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.password_title}>
+              Password
+              <span className={styles.required}>*</span>
+            </div>
+            <div className={styles.input_area}>
+              <Input
+                type="password"
+                autoComplete="current-password"
+                name="password"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.button_area}>
+              <Button size="large" className={styles.button}>
+                Sign In
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
 };
 
 export default SignIn;
